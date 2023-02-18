1f9ad645153cdf01b34d499cbd7408161f91a6f7
Tony Myers <tony@myers.co.nz>
2023-02-18 20:42:36 +1300

    Cleaning export and peerDeps

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,4 @@ import { VueFormGeneratorOptions } from "./types/VueFormGeneratorOptions";
 import type { ChildField, Field, Form } from "./types/VueFormGenerator"
 import VueFormGenerator from "./components/VueFormGenerator.vue";
 
-export {  ChildField, Field, Form, VueFormGenerator, VueFormGeneratorOptions, VueFormGeneratorPlugin };
+export { ChildField, Field, Form, VueFormGenerator, VueFormGeneratorOptions, VueFormGeneratorPlugin };
